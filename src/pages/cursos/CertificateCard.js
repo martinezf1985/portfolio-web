4489f94c7bf14c82eb3904c89e7aba0fa22daa94
martinezf1985 <martinezf1985@gmail.com>
2023-04-16 20:07:13 -0300

    subida

--- src/pages/cursos/CertificateCard.js
+++ src/pages/cursos/CertificateCard.js
@@ -1,3 +1,65 @@
+// import React,{useState} from 'react';
+// import { motion } from 'framer-motion';
+
+// const cardTransition = {
+//   type: 'spring',
+//   stiffness: 500,
+//   damping: 30
+// };
+
+// const cardVariants = {
+//   hover: {
+//     y: -5,
+//     boxShadow: '0 5px 20px rgba(148, 0, 211)',
+//     scale: 1.02
+//   },
+//   tap: {
+//     y: 0,
+//     boxShadow: '0 3px 10px rgba(148, 0, 211)',
+//     scale: 1
+//   },
+//   link: {
+//     cursor: 'pointer'
+//   }
+// };
+
+// const CertificateCard = ({ certificate }) => {
+//   const [error, setError] = useState(null); // estado para manejar los errores
+
+// const handleOnError = () => {
+// setError('Error al cargar la imagen'); // establecer el mensaje de error
+// };
+
+//   return (
+//     <motion.div
+//   className="certificate-card bg-black rounded-lg overflow-hidden shadow-md text-white"
+//   whileHover="hover"
+//   whileTap="tap"
+//   variants={cardVariants}
+//   transition={cardTransition}
+// >
+//   {certificate && certificate.image && (
+//     <img src={certificate.image} alt={certificate.title} className="mb-4" width="100%" onError={handleOnError} />
+//   )}
+//   <div className="p-4">
+//     <h3 className="text-xl font-medium mb-2">{certificate.title}</h3>
+//     <p className="text-white mb-4">{certificate.description}</p>
+//     <p className="text-white mb-2">
+//       {certificate && certificate.id && (
+//         <span className="text-white mb-2">
+//           <strong>ID:</strong> {certificate.id}
+//         </span>
+//       )}
+//     </p>
+//   </div>
+// </motion.div>
+
+//   );
+// };
+
+// export default CertificateCard;
+
+
 import React,{useState} from 'react';
 import { motion } from 'framer-motion';
 
@@ -26,34 +88,33 @@ const cardVariants = {
 const CertificateCard = ({ certificate }) => {
   const [error, setError] = useState(null); // estado para manejar los errores
 
-const handleOnError = () => {
-setError('Error al cargar la imagen'); // establecer el mensaje de error
-};
+  const handleOnError = () => {
+    setError('Error al cargar la imagen'); // establecer el mensaje de error
+  };
 
   return (
     <motion.div
-  className="certificate-card bg-black rounded-lg overflow-hidden shadow-md text-white"
-  whileHover="hover"
-  whileTap="tap"
-  variants={cardVariants}
-  transition={cardTransition}
->
-  {certificate && certificate.image && (
-    <img src={certificate.image} alt={certificate.title} className="mb-4" width="100%" onError={handleOnError} />
-  )}
-  <div className="p-4">
-    <h3 className="text-xl font-medium mb-2">{certificate.title}</h3>
-    <p className="text-white mb-4">{certificate.description}</p>
-    <p className="text-white mb-2">
-      {certificate && certificate.id && (
-        <span className="text-white mb-2">
-          <strong>ID:</strong> {certificate.id}
-        </span>
+      className="certificate-card bg-black rounded-lg overflow-hidden shadow-md text-white"
+      whileHover="hover"
+      whileTap="tap"
+      variants={cardVariants}
+      transition={cardTransition}
+    >
+      {certificate && certificate.image && (
+        <img src={certificate.image} alt={certificate.title} className="mb-4" width="100%" onError={handleOnError} />
       )}
-    </p>
-  </div>
-</motion.div>
-
+      <div className="p-4">
+        <h3 className="text-xl font-medium mb-2">{certificate.title ? certificate.title : 'No title provided'}</h3>
+        <p className="text-white mb-4">{certificate.description}</p>
+        <p className="text-white mb-2">
+          {certificate && certificate.id && (
+            <span className="text-white mb-2">
+              <strong>ID:</strong> {certificate.id}
+            </span>
+          )}
+        </p>
+      </div>
+    </motion.div>
   );
 };
 
