a53bf12a0f64a302843d03633e8faf0af4f00abb
martinezf1985 <martinezf1985@gmail.com>
2023-04-15 21:35:32 -0300

    surte

--- src/pages/cards/Cards.js
+++ src/pages/cards/Cards.js
@@ -37,7 +37,7 @@ function Cards(props) {
       setCurrentPage(nextPage);
       setProjectList([...projectList, ...projectsToAdd]);
     }
-  }, [currentPage, projects]);
+  }, [currentPage, projectList]);
 
   const handleClick = (event, link) => {
     event.preventDefault();
@@ -54,66 +54,63 @@ function Cards(props) {
   };
 
   return (
-    
     <div onScroll={handleScroll}>
-  
-
-  <div className="card-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
-    {projects.map((project) => (
-      <motion.a
-        href={project.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="card bg-black rounded-lg overflow-hidden shadow-md text-white"
-        key={project.id}
-        whileHover="hover"
-        whileTap="tap"
-        variants={cardVariants}
-        transition={cardTransition}
-        onClick={(event) => handleClick(event, project.link)}
-      >
-        <img src={project.image} alt={project.title} className="mb-4"/>
-        <div className="p-4">
-          <h3 className="text-xl font-medium mb-2">{project.title}</h3>
-          <p className="text-white mb-4">{project.description}</p>
-          <ul className="mb-4">
-            {project.technologies.map((technology) => (
-              <li className="inline-block bg-white  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" key={technology}>
-                {technology}
-              </li>
-            ))}
-          </ul>
-          <p className="text-white mb-2">
-            <strong>Start Date:</strong> {project.startDate}
-          </p>
-          {project.endDate && (
-          <p className="text-white mb-2">
-        <strong>End Date:</strong> {project.endDate}
-      </p>
-  )}
-          {project.location && (
-            <p className="text-white mb-2">
-              <strong>Location:</strong> {project.location}
-            </p>
-          )}
-          {project.company && (
-            <p className="text-white mb-2">
-              <strong>Company:</strong> {project.company}
-            </p>
-          )}
-          {project.type && (
-            <p className="text-white mb-2">
-              <strong>Type:</strong> {project.type}
-            </p>
-          )}
-        </div>
-      </motion.a>
-    ))}
-  </div>
-</div>
-
+      <div className="card-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
+        {projectList.map((project) => (
+          <motion.a
+            href={project.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="card bg-black rounded-lg overflow-hidden shadow-md text-white"
+            key={project.id}
+            whileHover="hover"
+            whileTap="tap"
+            variants={cardVariants}
+            transition={cardTransition}
+            onClick={(event) => handleClick(event, project.link)}
+          >
+            <img src={project.image} alt={project.title} className="mb-4"/>
+            <div className="p-4">
+              <h3 className="text-xl font-medium mb-2">{project.title}</h3>
+              <p className="text-white mb-4">{project.description}</p>
+              <ul className="mb-4">
+                {project.technologies.map((technology) => (
+                  <li className="inline-block bg-white  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" key={technology}>
+                    {technology}
+                  </li>
+                ))}
+              </ul>
+              <p className="text-white mb-2">
+                <strong>Start Date:</strong> {project.startDate}
+              </p>
+              {project.endDate && (
+                <p className="text-white mb-2">
+                  <strong>End Date:</strong> {project.endDate}
+                </p>
+              )}
+              {project.location && (
+                <p className="text-white mb-2">
+                  <strong>Location:</strong> {project.location}
+                </p>
+              )}
+              {project.company && (
+                <p className="text-white mb-2">
+                  <strong>Company:</strong> {project.company}
+                </p>
+              )}
+              {project.type && (
+                <p className="text-white mb-2">
+                  <strong>Type:</strong> {project.type}
+                </p>
+              )}
+            </div>
+          </motion.a>
+        ))}
+      </div>
+    </div>
   );
 }
+
 
 export default Cards;
 
